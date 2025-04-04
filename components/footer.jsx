caf909cc8df61086079e42ccc5cee9606a9414bb
Keith <34338831+keithrsialana@users.noreply.github.com>
2025-04-04 10:25:34 -0400

    updated contact page

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,19 +1,44 @@
+import { FaFacebookSquare } from "react-icons/fa";
+import { IoMail } from "react-icons/io5";
+import { FaInstagram } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 function Footer() {
 	return (
 		<footer
 			className="d-flex rounded align-items-center justify-content-center"
-			style={{ minHeight: "15vh" }}
+			style={{ minHeight: "15vh", paddingTop: "10px", paddingBottom: "10px" }}
 		>
 			<div className="row">
-				<div className="col d-flex align-items-center justify-content-end">
-					<p>Guelph Pinoy Basketball Club</p>
+				<div className="col d-flex flex-column align-items-center">
+					<div className="row">
+						<div className="col">
+							<p>Guelph Pinoy Basketball Club</p>{" "}
+						</div>
+					</div>
+
+					<div className="row">
+						<div className="col">
+							<Link to="mailto:thunderist17@gmail.com">
+								<IoMail style={{ color: "#F25A4C", fontSize: "2em" }} />
+							</Link>
+						</div>
+						<div className="col">
+							<Link to="https://www.facebook.com/GuelphPinoyBasketballClub">
+								<FaFacebookSquare
+									style={{ color: "#6081C4", fontSize: "2em" }}
+								/>
+							</Link>
+						</div>
+						<div className="col">
+							<Link to="https://www.instagram.com/gpbc.official/">
+								<FaInstagram style={{ color: "#C41662", fontSize: "2em" }} />
+							</Link>
+						</div>
+					</div>
 				</div>
 				<div className="col d-flex align-items-center justify-content-center">
-					<img
-						src="Logo.png"
-						alt="GPBC Logo"
-						width="50%"
-					/>
+					<img src="Logo.png" alt="GPBC Logo" width="50%" />
 				</div>
 				<div className="col d-flex align-items-center justify-content-start">
 					<p>GPBC © 2025</p>
--- components/pages/contact.jsx
+++ components/pages/contact.jsx
@@ -1,11 +1,29 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaFacebookSquare } from "react-icons/fa";
+import { IoMail } from "react-icons/io5";
+import { FaInstagram } from "react-icons/fa";
 
 function Contact() {
-
 	return (
-		<>
+		<div className="contentmargin">
 			<h1>Contact Us</h1>
-		</>
+			<h3>
+				If you have any inqueries about how to join the club, prices, or any
+				other questions, please don't hesitate to contact us.
+				<br /> You can reach us through email, or send a message to our social
+				media accounts
+			</h3>
+			<Link to="mailto:thunderist17@gmail.com">
+				<IoMail style={{ color: "#F25A4C", fontSize: "7em" }} />
+			</Link>
+			<Link to="https://www.facebook.com/GuelphPinoyBasketballClub">
+				<FaFacebookSquare style={{ color: "#6081C4", fontSize: "7em" }} />
+			</Link>
+			<Link to="https://www.instagram.com/gpbc.official/">
+				<FaInstagram style={{ color: "#C41662", fontSize: "7em" }} />
+			</Link>
+		</div>
 	);
 }
 
